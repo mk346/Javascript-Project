2babaa01e69a8d03d6550416189d2f39006a2a71
MJEND7 <60278279+MJEND7@users.noreply.github.com>
2021-05-18 11:37:56 +1000

    ✔ Try this i just optimized you code
    hello i just optimized you code try it out, and Keep on Coding 👍

--- Java Script/home.js
+++ Java Script/home.js
@@ -1,30 +1,31 @@
 /*
-//console.log('hello');
-//alert('Whats up!!');
-//var b = 'smoothie';
-//console.log(b);
-//var age = prompt('what is your age?'); 
-//document.getElementById('someText').innerHTML = age;
+console.log('hello');
+alert('Whats up!!');
+const b = 'smoothie';
+console.log(b);
+const age = prompt('what is your age?'); 
+document.getElementById('someText').innerHTML = age;
 
-//functions declaration
+functions declaration
 function fun(){
-   // console.log('this is a function');
+   console.log('this is a function');
 }
-//function call
-//fun();
+function call
+fun();
 function greeting(){
-   // var name = prompt('what is your name');
-    //var result = 'Hello' + '' + name;
+    let name = prompt('what is your name');
+    let result = 'Hello' + '' + name;
     //console.log(result);
 }
-//greeting();
-//while loop
-//num=0;
-/*while(num < 100){
- num+=2
- console.log(num);
+greeting();
+
+  //while loop
+    num=0;
+  while(num < 100){
+    num+=2
+    console.log(num);
+}
+  //for loop
+  for(let num=0; num<=100; num++){
+    console.log(num);
 }*/
-//for loop
-//for(let num=0; num<=100; num++){
-    //console.log(num);
-//}*/
